e81fa57e481e0fed04a13e5b26cf2dec44b1e23f
YOBLANA <salahovtagir@mail.ru>
2025-07-14 20:14:38 +0300

    анимация звёзд, десктоп и мобилка

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy, HostListener } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { HeaderComponent } from '../header/header.component';
@@ -9,4 +9,47 @@ import { HeaderComponent } from '../header/header.component';
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss',
 })
-export class MainComponent {}
+export class MainComponent implements OnInit, OnDestroy {
+  isSmallScreen = false;
+
+  ngOnInit() {
+    this.checkScreenSize();
+  }
+
+  ngOnDestroy() {
+    // Clean up if needed
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.checkScreenSize();
+  }
+
+  @HostListener('window:scroll', ['$event'])
+  onScroll(event: any) {
+    this.animateStars();
+  }
+
+  private checkScreenSize() {
+    this.isSmallScreen = window.innerWidth <= 360;
+  }
+
+  private animateStars() {
+    const stars = document.querySelectorAll('.fivestars-img');
+    stars.forEach((star, index) => {
+      const element = star as HTMLElement;
+      element.style.transform = 'scale(1.8)';
+
+      if (index === 0) {
+        // top star
+        element.style.transform += ' translateX(100px)';
+      } else if (index === 1) {
+        // middle star
+        element.style.transform += ' translateX(-100px)';
+      } else if (index === 2) {
+        // bottom star
+        element.style.transform += ' translateX(100px)';
+      }
+    });
+  }
+}
